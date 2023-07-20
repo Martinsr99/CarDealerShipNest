7b52d3dec05b441ceec1d7213e5c70821caa1db4
Martinsr99 <silesreche.martin@gmail.com>
2023-07-20 16:02:47 +0200

    feat: get car by id and create car service

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
+    constructor(private readonly carsService:CarsService){
+
+    }
+
     @Get()
     getAllcars() {
-        return ['Kia']
+        return this.carsService.findAll()
     }
+
+    @Get(':id')
+    getCarById(@Param('id') id) {
+        return this.carsService.findCarById(+id)
+    }
+
+
 }
