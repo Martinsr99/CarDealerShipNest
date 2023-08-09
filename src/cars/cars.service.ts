dad8ccf6935cd035213d7bab787ad9c377412ad1
Martinsr99 <silesreche.martin@gmail.com>
2023-08-09 14:13:16 +0200

    feat: end CRUD for cars

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,32 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
 
 @Injectable()
 export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: "Toyota",
+      model: "Corolla",
+    },
+    {
+      id: 2,
+      brand: "Honda",
+      model: "Civic",
+    },
+    {
+      id: 3,
+      brand: "Jeep",
+      model: "Cherokee",
+    },
+  ];
 
-    private cars = [
-        {
-            id:1,
-            brand:'Toyota',
-            model:'Corolla'
-        },
-        {
-            id:2,
-            brand:'Honda',
-            model:'Civic'
-        },
-        {
-            id:3,
-            brand:'Jeep',
-            model:'Cherokee'
-        },
-    ]
+  findAll() {
+    return this.cars;
+  }
 
-    findAll(){
-        return this.cars
-    }
+  findCarById(id: number) {
+    const car = this.cars.find(car => car.id === id);
 
-    findCarById(id:number){
-        return this.cars.find(car => car.id === id)
-    }
+    if (!car) throw new NotFoundException();
 
+    return car
+  }
 }
