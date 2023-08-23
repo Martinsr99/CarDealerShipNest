f475e1c0538f462e2bbbbc47a5b636e6a3124676
Martinsr99 <silesreche.martin@gmail.com>
2023-08-23 13:49:34 +0200

    feat: end CRUD for brands

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -42,11 +42,22 @@ export class BrandsService {
     return brand
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDB = this.findOne(id)
+
+    this.brands = this.brands.map(brand => {
+      if(brand.id === id){
+        brandDB.updatedAt = new Date().getTime()
+        brandDB = {...brandDB,...updateBrandDto}
+        return brandDB
+      }
+      return brand
+    })
+
+    return brandDB
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    this.brands = this.brands.filter(brand => brand.id === id)
   }
 }
